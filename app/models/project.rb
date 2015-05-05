72e052ec1bf39da664d4e27fe1e5c20c07ccda30
Marc Bluemner <marc.bluemner@gmail.com>
2015-05-05 17:00:55 +0200

    feature pass!

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project
 
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
+
+  def self.velocity_length_in_days
+    21
+  end
 
   def initialize
     @tasks = []
@@ -20,5 +24,22 @@ class Project
 
   def remaining_size
     incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / Project.velocity_length_in_days
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    return false if projected_days_remaining.nan?
+    (Date.today + projected_days_remaining) <= due_date
   end
 end
