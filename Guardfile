f448599060db05211b41c4f6fe6da1cc50581007
Marc Bluemner <marc.bluemner@gmail.com>
2015-05-06 18:02:12 +0200

    project/index

--- Guardfile
+++ Guardfile
@@ -104,6 +104,7 @@ guard :rspec, cmd: "bundle exec rspec" do
 
   # Turnip features and steps
   watch(%r{^spec/acceptance/(.+)\.feature$})
+  watch(%r{^app/views/**/(.+)\.html.erb$})
   watch(%r{^spec/acceptance/steps/(.+)_steps\.rb$}) do |m|
     Dir[File.join("**/#{m[1]}.feature")][0] || "spec/acceptance"
   end
--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -1,13 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe "adding projects" do
-  it 'allows a user to create a project with tasks' do
+  it "allows a user to create a project with tasks" do
     visit new_project_path
-    fill_in 'Name', with: 'Project Runway'
-    fill_in 'Tasks', with: 'Task 1:3\nTask2:5'
-    click_on('Create Project')
+    fill_in "Name", with: "Project Runway"
+    fill_in "Tasks", with: "Task 1:3\nTask 2:5"
+    click_on("Create Project")
     visit projects_path
-    expect(page).to have_content('Project Runway')
-    expect(page).to have_content('8')
+    @project = Project.find_by_name("Project Runway")
+    expect(page).to have_selector(
+        "#project_#{@project.id} .name", text: "Project Runway")
+    expect(page).to have_selector(
+        "#project_#{@project.id} .total-size", text: "8")
   end
 end
